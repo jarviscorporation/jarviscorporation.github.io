c0dd291b8e64d361f6781a2d67d86042fef2164a
jarvis615 <48697552+jarvis615@users.noreply.github.com>
2019-03-21 21:07:41 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-menu.onclick = function muFunction() {
-	var x = document.getElementById('myTopnav');
-
-	if (x.className == "topnav")  {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
