f11ec78053a9bbe81baf47e6f57321ca88714aa1
jarvis615 <48697552+jarvis615@users.noreply.github.com>
2019-03-21 19:34:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 menu.onclick = function muFunction() {
 	var x = document.getElementById('myTopnav');
 
@@ -7,14 +6,3 @@ menu.onclick = function muFunction() {
 	} else {
 		x.className = "topnav";
 	}
-=======
-menu.onclick = function muFunction() {
-	var x = document.getElementById('myTopnav');
-
-	if (x.className === "topnav")  {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
->>>>>>> 9401c2d3c42fe340368f50af05a7e7e63914ea9e
-}
