d00dc1807725656bfed03170eb76a8c9548096a2
jarvis615 <48697552+jarvis615@users.noreply.github.com>
2019-03-21 18:35:39 +0200

    fullsite

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+menu.onclick = function muFunction() {
+	var x = document.getElementById('myTopnav');
+
+	if (x.className == "topnav")  {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+=======
 menu.onclick = function muFunction() {
 	var x = document.getElementById('myTopnav');
 
@@ -6,4 +16,5 @@ menu.onclick = function muFunction() {
 	} else {
 		x.className = "topnav";
 	}
+>>>>>>> 9401c2d3c42fe340368f50af05a7e7e63914ea9e
 }
